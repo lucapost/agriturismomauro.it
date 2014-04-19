9fc61f0af8348ffc3291406dacdbab6625d4a80c
Luca Postregna <luca.postregna@gmail.com>
2014-04-19 18:42:00 +0200

    fix

--- templates/it.py
+++ templates/it.py
@@ -6,7 +6,7 @@ description = "Agriturismo ad Oleis di Manzano"
 title = "Agriturismo Mauro - ristoro agrituristico e vendita vini"
 site_name = "Agriturismo Mauro"
 subname = "ad Oleis di Manzano"
-site_desc = "Cucina tipica e vini friulani"
+site_desc = "Cucina contadina e vini friulani"
 src_dir = "src/it"
 dst_dir = "dst"
 execfile('templates/default.py')
