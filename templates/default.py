6745ed0e5c6bd2fd933a48bb631170f7c0b08822
Luca Postregna <luca.postregna@gmail.com>
2014-04-23 10:51:06 +0200

    fix

--- templates/default.py
+++ templates/default.py
@@ -37,27 +37,27 @@ def header(node):
 			<div id="title" class="grid_9">
 				<a href="/" title="home page">
 					<h1>''' + site_name + '''</h1>
-					<h1>''' + subname + '''</h1>
+	<!--				<h1>''' + subname + '''</h1> -->
 				</a>
 				<h2>''' + site_desc + '''</h2>
-			</div>
-			<div id="hright" class="grid_7 clearfix">
-				<div id="buttons">
-					<div id="facebook">
-						<a href="https://www.facebook.com/AgriturismoMauroVini" title="Agriturismo Mauro su Facebook">
-							<p class="hide">facebook</p>
-						</a>
-					</div>
-					<div id="tripadvisor">
+        <!--				<div id="buttons">
+					<div id="tripadvisor" class="alpha grid_2">
 						<a href="#" title="Agriturismo Mauro su TripAdvisor">
 							<p class="hide">tripadvisor</p>
 						</a>
 					</div> 
-				</div>
+					<div id="facebook" class="omega grid_1">
+						<a href="https://www.facebook.com/AgriturismoMauroVini" title="Agriturismo Mauro su Facebook">
+							<p class="hide">facebook</p>
+						</a>
+					</div>
+				</div> -->
+			</div>
+			<div id="hright" class="grid_7 clearfix">
 				<div id="contacts">
 					di Roberto Mauro e Dolores Pascolo<br/><br/>
 					<a title="Agriturismo Mauro su Google Maps" href="">Via Vittorio Veneto 1, Fraz. Oleis<br/>Manzano, 33044 Udine</a><br/><br/>
-					tel: +39 3803516322<br/>
+					tel: +39 3803516322, +39 0432740029<br/>
 				     	mail: <a href="#" title="indirizzo email dell'agriturismo mauro">info@agriturismomauro.it</a>
 				</div>
 			</div>
--- templates/it.py
+++ templates/it.py
@@ -6,7 +6,7 @@ description = "Agriturismo ad Oleis di Manzano"
 title = "Agriturismo Mauro - Cucina e vendita vini"
 site_name = "Agriturismo Mauro"
 subname = "ad Oleis di Manzano"
-site_desc = "Cucina friulana e vini dei Colli Orientali"
+site_desc = "Cucina tipica friulana <br/>Vini dei Colli Orientali di Ronchi del Sole"
 src_dir = "src/it"
 dst_dir = "dst"
 execfile('templates/default.py')
